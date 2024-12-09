c643fd58c1afa2c6cdfb530ecc52133f63299739
Nicolaslucero01 <103524887+Nicolaslucero01@users.noreply.github.com>
2024-12-08 23:43:38 -0300

    Add files via upload

---
+++ assets/scripts/index.js
@@ -0,0 +1,98 @@
+const productsContainer = document.querySelector(".products__cards");
+const showMoreBtn = document.querySelector(".btn__loaded");
+const categoryFilter = document.getElementById("categoryFilter");
+const searchBar = document.getElementById("searchBar");
+
+let loadedProducts = 12;
+const productsPerPage = 12;
+
+const formatPrice = (price) => {
+  const hasCents = price % 1 !== 0;
+  return (hasCents ? price.toFixed(2) : price.toString())
+    .replace(".", ",")
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+};
+
+const createProductTemplate = (product) => {
+  const { id, nombre, imagen, precio } = product;
+  return `<div class="card">
+    <div class="card__img">
+      <img src="${imagen}" alt="${nombre}">
+    </div>
+
+    <div class="card__content">
+      <h3>${nombre}</h3>
+      <span>$${formatPrice(precio)} USD</span>
+    </div>
+
+    <div class="card__buy">
+      <button class="btn" data-id='${id}' data-name="${nombre}" data-image="${imagen}" data-price="${precio}">
+        Agregar al carrito
+      </button>
+    </div>
+  </div>`;
+};
+
+const renderProducts = (productList) => {
+  productsContainer.innerHTML = "";
+  productList.forEach((product) => {
+    productsContainer.innerHTML += createProductTemplate(product);
+  });
+};
+
+const showMoreProducts = () => {
+  loadedProducts += productsPerPage;
+  const nextProducts = appState.filteredProducts.slice(0, loadedProducts);
+  renderProducts(nextProducts);
+
+  if (loadedProducts >= appState.filteredProducts.length) {
+    showMoreBtn.style.display = "none";
+  }
+};
+
+const applyFilter = () => {
+  const selectedCategory = categoryFilter.value;
+  let filteredProducts = appState.products;
+
+  if (selectedCategory !== "Todos") {
+    filteredProducts = filteredProducts.filter(
+      (product) => product.category === selectedCategory
+    );
+  }
+
+  appState.filteredProducts = filteredProducts.filter((product) =>
+    product.nombre.toLowerCase().includes(searchBar.value.toLowerCase())
+  );
+
+  loadedProducts = productsPerPage;
+
+  const visibleProducts = appState.filteredProducts.slice(0, loadedProducts);
+
+  renderProducts(visibleProducts);
+
+  if (appState.filteredProducts.length > productsPerPage) {
+    showMoreBtn.style.display = "inline-block";
+  } else {
+    showMoreBtn.style.display = "none";
+  }
+};
+
+const applySearch = () => {
+  applyFilter();
+};
+
+const appState = {
+  products: productsData,
+  filteredProducts: productsData,
+};
+
+const init = () => {
+  const initialProducts = appState.filteredProducts.slice(0, loadedProducts);
+  renderProducts(initialProducts);
+
+  showMoreBtn.addEventListener("click", showMoreProducts);
+  categoryFilter.addEventListener("change", applyFilter);
+  searchBar.addEventListener("input", applySearch);
+};
+
+init();
---
+++ assets/scripts/products.js
@@ -0,0 +1,356 @@
+const productsData = [
+  {
+    id: 1,
+    nombre: "Samsung A15 4GB 128GB",
+    precio: 210,
+    imagen: "./assets/img/a15.png",
+    category: "Samsung",
+  },
+  {
+    id: 2,
+    nombre: "Samsung A15 6GB 128GB",
+    precio: 220,
+    imagen: "./assets/img/a15.png",
+    category: "Samsung",
+  },
+  {
+    id: 3,
+    nombre: "Samsung A15 8GB 256GB",
+    precio: 260,
+    imagen: "./assets/img/a15.png",
+    category: "Samsung",
+  },
+
+  {
+    id: 39,
+    nombre: "Samsung A15 5G 8GB 256GB",
+    precio: 280,
+    imagen: "./assets/img/a15.png",
+    category: "Samsung",
+  },
+  {
+    id: 4,
+    nombre: "Samsung A25 5G 8GB 256GB",
+    precio: 280,
+    imagen: "./assets/img/a25.png",
+    category: "Samsung",
+  },
+  {
+    id: 5,
+    nombre: "Samsung A25 5G 8GB 128GB",
+    precio: 300,
+    imagen: "./assets/img/a25.png",
+    category: "Samsung",
+  },
+  {
+    id: 6,
+    nombre: "Samsung A35 5G 8GB 128GB",
+    precio: 375,
+    imagen: "./assets/img/a35.png",
+    category: "Samsung",
+  },
+  {
+    id: 7,
+    nombre: "Samsung A35 5G 8GB 256GB",
+    precio: 392,
+    imagen: "./assets/img/a35.png",
+    category: "Samsung",
+  },
+  {
+    id: 8,
+    nombre: "Samsung A55 5G 8GB 128GB",
+    precio: 450,
+    imagen: "./assets/img/a55.png",
+    category: "Samsung",
+  },
+  {
+    id: 9,
+    nombre: "Samsung A55 5GB 8GB 256GB",
+    precio: 475,
+    imagen: "./assets/img/a55.png",
+    category: "Samsung",
+  },
+  {
+    id: 10,
+    nombre: "Samsung S23 Ultra 12GB 256GB",
+    precio: 1060,
+    imagen: "./assets/img/s23Ultra.png",
+    category: "Samsung",
+  },
+  {
+    id: 11,
+    nombre: "Samsung S23 Ultra 12GB 512GB",
+    precio: 1100,
+    imagen: "./assets/img/s23Ultra.png",
+    category: "Samsung",
+  },
+  {
+    id: 12,
+    nombre: "Samsung S23 FE 8GB 256GB",
+    precio: 590,
+    imagen: "./assets/img/s23Fe.png",
+    category: "Samsung",
+  },
+  {
+    id: 13,
+    nombre: "Samsung S24 FE 8GB 256GB",
+    precio: 800,
+    imagen: "./assets/img/s24Fe.png",
+    category: "Samsung",
+  },
+  {
+    id: 14,
+    nombre: "Samsung S24 8GB 128GB",
+    precio: 850,
+    imagen: "./assets/img/s24.png",
+    category: "Samsung",
+  },
+  {
+    id: 15,
+    nombre: "Samsung S24 8GB 256GB",
+    precio: 900,
+    imagen: "./assets/img/s24.png",
+    category: "Samsung",
+  },
+  {
+    id: 16,
+    nombre: "Xiaomi 13C 4GB 128GB",
+    precio: 200,
+    imagen: "./assets/img/xiaomi13c.png",
+    category: "Xiaomi",
+  },
+  {
+    id: 17,
+    nombre: "Xiaomi 14C 4GB 128GB",
+    precio: 215,
+    imagen: "./assets/img/xiaomi14c.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 18,
+    nombre: "Xiaomi Redmi Note 13 4G 6GB 128GB",
+    precio: 240,
+    imagen: "./assets/img/xiaomiNote134g.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 19,
+    nombre: "Xiaomi Redmi Note 13 4G 8GB 128GB",
+    precio: 250,
+    imagen: "./assets/img/xiaomiNote134g.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 20,
+    nombre: "Xiaomi Redmi Note 13 4G 8GB 256GB",
+    precio: 270,
+    imagen: "./assets/img/xiaomiNote134g.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 21,
+    nombre: "Xiaomi Redmi Note 13 PRO 4G 8GB 256GB",
+    precio: 290,
+    imagen: "./assets/img/xiaomi13pro4g.png",
+
+    category: "Xiaomi",
+  },
+
+  {
+    id: 22,
+    nombre: "Xiaomi Redmi Note 13 PRO 4G 12GB 256GB",
+    precio: 310,
+    imagen: "./assets/img/xiaomi13pro4g.png",
+
+    category: "Xiaomi",
+  },
+
+  {
+    id: 23,
+    nombre: "Xiaomi Redmi Note 13 PRO 4G 12GB 512GB",
+    precio: 350,
+    imagen: "./assets/img/xiaomi13pro4g.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 24,
+    nombre: "Xiaomi Redmi Note 13 5G 6GB 128GB",
+    precio: 285,
+    imagen: "./assets/img/xiaomiNote135g.png",
+    category: "Xiaomi",
+  },
+  {
+    id: 25,
+    nombre: "Xiaomi Redmi Note 13 5G 8GB 256GB",
+    precio: 320,
+    imagen: "./assets/img/xiaomiNote135g.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 26,
+    nombre: "Xiaomi Redmi Note 13 PRO 5G 8GB 256GB",
+    precio: 380,
+    imagen: "./assets/img/xiaomi13pro5g.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 27,
+    nombre: "Xiaomi Redmi Note 13 PRO 5G 8GB 512GB",
+    precio: 440,
+    imagen: "./assets/img/xiaomi13pro5g.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 28,
+    nombre: "Xiaomi Redmi Note 13 PRO + 5G 8GB 256GB",
+    precio: 440,
+    imagen: "./assets/img/xiaomi13pro+5g.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 29,
+    nombre: "Xiaomi Redmi Note 13 PRO + 5G 8GB 512GB",
+    precio: 490,
+    imagen: "./assets/img/xiaomi13pro+5g.png",
+    category: "Xiaomi",
+  },
+
+  {
+    id: 30,
+    nombre: "Motorola Moto G24 8GB 128GB",
+    precio: 210,
+    imagen: "./assets/img/motog24.png",
+    category: "Motorola",
+  },
+
+  {
+    id: 31,
+    nombre: "Motorola Moto G24 Power 8GB 256GB",
+    precio: 230,
+    imagen: "./assets/img/motog24power.png",
+    category: "Motorola",
+  },
+
+  {
+    id: 32,
+    nombre: "Motorola Moto G54 5G 8GB 256GB",
+    precio: 280,
+    imagen: "./assets/img/motog54.png",
+    category: "Motorola",
+  },
+
+  {
+    id: 33,
+    nombre: "Motorola Moto G73 8GB 256GB",
+    precio: 280,
+    imagen: "./assets/img/motog73.png",
+    category: "Motorola",
+  },
+
+  {
+    id: 34,
+    nombre: "Motorola Moto G84 5G 8GB 256GB",
+    precio: 350,
+    imagen: "./assets/img/motog84.png",
+    category: "Motorola",
+  },
+
+  {
+    id: 35,
+    nombre: "Motorola Moto G84 5G 12GB 256GB",
+    precio: 370,
+    imagen: "./assets/img/motog84.png",
+    category: "Motorola",
+  },
+
+  {
+    id: 36,
+    nombre: "Motorola Moto G85 5G 8GB 256GB",
+    precio: 390,
+    imagen: "./assets/img/motog85.png",
+    category: "Motorola",
+  },
+
+  {
+    id: 37,
+    nombre: "Motorola Moto Edge 50 Fusion 5G 8GB 256GB",
+    precio: 500,
+    imagen: "./assets/img/motoedge50.png",
+    category: "Motorola",
+  },
+
+  {
+    id: 38,
+    nombre: "Xbox Series X 1TB",
+    precio: 750,
+    imagen: "./assets/img/xboxseriesx.png",
+    category: "Consolas",
+  },
+
+  {
+    id: 39,
+    nombre: "Xbox Series S 512GB",
+    precio: 550,
+    imagen: "./assets/img/xboxseriess.png",
+    category: "Consolas",
+  },
+
+  {
+    id: 40,
+    nombre: "Xbox Series X 1TB Digital Blanca ",
+    precio: 750,
+    imagen: "./assets/img/xboxseriesxwhite.png",
+    category: "Consolas",
+  },
+
+  {
+    id: 41,
+    nombre: "Xbox Series S 1TB",
+    precio: 650,
+    imagen: "./assets/img/xboxseriessblack.png",
+    category: "Consolas",
+  },
+
+  {
+    id: 42,
+    nombre: "Play Station 5 Slim 1TB",
+    precio: 840,
+    imagen: "./assets/img/ps5.png",
+    category: "Consolas",
+  },
+
+  {
+    id: 43,
+    nombre: "Play Station 5 Slim 1TB Digital",
+    precio: 790,
+    imagen: "./assets/img/ps5digital.png",
+    category: "Consolas",
+  },
+
+  {
+    id: 44,
+    nombre: "Nintendo Switch OLED 64GB",
+    precio: 470,
+    imagen: "./assets/img/switch.png",
+    category: "Consolas",
+  },
+
+  {
+    id: 45,
+    nombre: "Nintendo Switch Lite",
+    precio: 290,
+    imagen: "./assets/img/switchlite.png",
+    category: "Consolas",
+  },
+];
+
+const sortedProducts = productsData.sort((a, b) => b.precio - a.precio);
